10aafff1a0dda36cac385b29449a9664a34a2c32
abp437 <abp437@gmail.com>
2021-08-13 02:23:25 +0530

    Minor Improvements

--- guess_number_game.rb
+++ guess_number_game.rb
@@ -3,20 +3,19 @@
 
 puts "Welcome to 'Guess My Number!'"
 print "What's your name? "
-user_name = gets
-user_name.chomp!
+user_name = gets.chomp!
 
 puts "Welcome, #{user_name}!"
 
-num_to_guess = rand(100)
+num_to_guess = rand(100) + 1 # Since 'rand' generates one number below the specified range, we need to add 1 to it.
 chances_to_guess = 10
 user_guessed_correct = false
 
-puts "Guess a number from 0 - 100 in #{chances_to_guess} chances"
+puts "I've got a random number in the range of 1 - 100."
+puts "Can you guess it in #{chances_to_guess} chances?"
 
 1.upto(chances_to_guess) do |i|
-  guess = gets
-  user_guess = guess.to_i
+  user_guess = gets.to_i
 
   if user_guess === num_to_guess
     user_guessed_correct = true
