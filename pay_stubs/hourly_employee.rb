4a5c0e6363b232eac86192a52bcf08e1cddc5972
abp437 <abp437@gmail.com>
2021-08-29 23:27:23 +0530

    Add class methods

--- pay_stubs/hourly_employee.rb
+++ pay_stubs/hourly_employee.rb
@@ -24,7 +24,23 @@ class HourlyEmployee < Employee
     # attribute writer methods, which have validations defined in them.
     super(name)
     self.hourly_wage = hourly_wage
-    self.hour_per_week = hours_per_week
+    self.hours_per_week = hours_per_week
+  end
+
+  # The ideal solution to create different types of employees is to actually create separate
+  # classes for each type of employee. Since each employee will have different roles and responsibilities.
+  # But just for demonstration we are using class methods.
+
+  def self.cashier(name)
+    self.new(name, 12.75, 25)
+  end
+
+  def self.janitor(name)
+    self.new(name, 10.5, 20)
+  end
+
+  def self.security_guard(name)
+    self.new(name, 19.25, 30)
   end
 
   def print_pay_stub
@@ -33,3 +49,9 @@ class HourlyEmployee < Employee
     puts "Pay for this period: $#{pay_for_period}"
   end
 end
+
+jason = HourlyEmployee.security_guard("Jason Jackson")
+jason.print_pay_stub
+
+chloe = HourlyEmployee.cashier("Chloe Jackson")
+chloe.print_pay_stub
